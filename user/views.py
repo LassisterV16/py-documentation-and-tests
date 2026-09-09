376bd44560e4a614ce50073a6b246831c8bda4af
lassisterv16 <lassisterv16@gmail.com>
2026-09-09 14:34:16 +0200

    change github python version

--- user/views.py
+++ user/views.py
@@ -1,11 +1,8 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer, AuthTokenSerializer
+from user.serializers import UserSerializer
 
 
 @extend_schema(summary="Create new user")
@@ -15,7 +12,6 @@ class CreateUserView(generics.CreateAPIView):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
